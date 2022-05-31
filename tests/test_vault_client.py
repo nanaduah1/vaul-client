5a64d759ea086a163564ead07064cd6d4aa37a7f
Nana Duah <gabano2005@gmail.com>
2022-05-30 22:42:50 -0400

    Added release workflow

--- tests/test_vault_client.py
+++ tests/test_vault_client.py
@@ -1,5 +1,6 @@
-from vault_client import __version__
+from vaultclient import __version__
 
+from vaultclient.client import VaultClient
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.2.0'
